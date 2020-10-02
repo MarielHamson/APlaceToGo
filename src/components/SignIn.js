487341974cfe8dff9af7041b71f4e55d51be5213
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-10-02 12:10:59 -0700

    side sign in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -14,7 +14,7 @@ import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 
 function Copyright() {
 	return (
@@ -30,8 +30,21 @@ function Copyright() {
 }
 
 const useStyles = makeStyles((theme) => ({
+	root: {
+		height: '100vh',
+	},
+	image: {
+		backgroundImage: 'url(https://source.unsplash.com/featured/?{bathroom})',
+		backgroundRepeat: 'no-repeat',
+		backgroundColor:
+			theme.palette.type === 'light'
+				? theme.palette.grey[50]
+				: theme.palette.grey[900],
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+	},
 	paper: {
-		marginTop: theme.spacing(8),
+		margin: theme.spacing(8, 4),
 		display: 'flex',
 		flexDirection: 'column',
 		alignItems: 'center',
@@ -101,48 +114,50 @@ function SignIn() {
 	return (
 		<React.Fragment>
 			<Header />
-			<Container component="main" maxWidth="xs">
+			<Grid container component="main" className={classes.root}>
 				<CssBaseline />
-				<div className={classes.paper}>
-					<Avatar className={classes.avatar}>
-						<LockOutlinedIcon />
-					</Avatar>
-					<Typography component="h1" variant="h5">
-						Sign Up
-					</Typography>
-					<form className={classes.form} noValidate onSubmit={doSignUp}>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							id="email"
-							label="Email Address"
-							name="email"
-							autoComplete="email"
-							autoFocus
-						/>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-						/>
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							color="primary"
-							className={classes.submit}
-						>
+				<Grid item xs={false} sm={4} md={7} className={classes.image} />
+				<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+					<div className={classes.paper}>
+						<Avatar className={classes.avatar}>
+							<LockOutlinedIcon />
+						</Avatar>
+						<Typography component="h1" variant="h5">
 							Sign Up
-						</Button>
-						{/* <Button
+						</Typography>
+						<form className={classes.form} noValidate onSubmit={doSignUp}>
+							<TextField
+								variant="outlined"
+								margin="normal"
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								autoComplete="email"
+								autoFocus
+							/>
+							<TextField
+								variant="outlined"
+								margin="normal"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								autoComplete="current-password"
+							/>
+							<Button
+								type="submit"
+								fullWidth
+								variant="contained"
+								color="primary"
+								className={classes.submit}
+							>
+								Sign Up
+							</Button>
+							{/* <Button
 							type="submit"
 							fullWidth
 							variant="contained"
@@ -152,64 +167,65 @@ function SignIn() {
 						>
 							Sign out
 						</Button> */}
-					</form>
-					<Typography component="h1" variant="h5">
-						Sign In
-					</Typography>
-					<form className={classes.form} noValidate onSubmit={doSignIn}>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							id="email"
-							label="Email Address"
-							name="email"
-							autoComplete="email"
-							autoFocus
-						/>
-						<TextField
-							variant="outlined"
-							margin="normal"
-							required
-							fullWidth
-							name="password"
-							label="Password"
-							type="password"
-							id="password"
-							autoComplete="current-password"
-						/>
-						<FormControlLabel
-							control={<Checkbox value="remember" color="primary" />}
-							label="Remember me"
-						/>
-						<Button
-							type="submit"
-							fullWidth
-							variant="contained"
-							color="primary"
-							className={classes.submit}
-						>
+						</form>
+						<Typography component="h1" variant="h5">
 							Sign In
-						</Button>
+						</Typography>
+						<form className={classes.form} noValidate onSubmit={doSignIn}>
+							<TextField
+								variant="outlined"
+								margin="normal"
+								required
+								fullWidth
+								id="email"
+								label="Email Address"
+								name="email"
+								autoComplete="email"
+								autoFocus
+							/>
+							<TextField
+								variant="outlined"
+								margin="normal"
+								required
+								fullWidth
+								name="password"
+								label="Password"
+								type="password"
+								id="password"
+								autoComplete="current-password"
+							/>
+							<FormControlLabel
+								control={<Checkbox value="remember" color="primary" />}
+								label="Remember me"
+							/>
+							<Button
+								type="submit"
+								fullWidth
+								variant="contained"
+								color="primary"
+								className={classes.submit}
+							>
+								Sign In
+							</Button>
+						</form>
 						<Grid container>
 							<Grid item xs>
 								<Link href="#" variant="body2">
 									Forgot password?
 								</Link>
-							</Grid>
-							<Grid item>
-								<Link href="#" variant="body2">
-									{"Don't have an account? Sign Up Above"}
-								</Link>
+								<Grid item>
+									<Link href="#" variant="body2">
+										{"Don't have an account? Sign Up Above"}
+									</Link>
+								</Grid>
 							</Grid>
 						</Grid>
-					</form>
-				</div>
-				<Box mt={8}>
-					<Copyright />
-				</Box>
-			</Container>
+						<Box mt={8}>
+							<Copyright />
+						</Box>
+					</div>
+				</Grid>
+			</Grid>
 		</React.Fragment>
 	);
 }
