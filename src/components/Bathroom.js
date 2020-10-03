f650750fb6363e6c976bc46ee5b3b38514d49fce
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-10-02 17:36:54 -0700

    add material cards

--- src/components/Bathroom.js
+++ src/components/Bathroom.js
@@ -1,21 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Card from 'react-bootstrap/Card';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+	root: {
+		minWidth: 275,
+	},
+	bullet: {
+		display: 'inline-block',
+		margin: '0 2px',
+		transform: 'scale(0.8)',
+	},
+	title: {
+		fontSize: 14,
+	},
+	pos: {
+		marginBottom: 12,
+	},
+});
 
 function Bathroom(props) {
-	const myStyledList = {
-		listStyle: 'none',
-		cursor: 'pointer',
-	};
+	// 	const myStyledList = {
+	// 		listStyle: 'none',
+	// 		cursor: 'pointer',
+	// 	};
+	const classes = useStyles();
+	// const bull = <span className={classes.bullet}>•</span>;
+
 	return (
 		<React.Fragment>
-			<div
-				style={myStyledList}
-				onClick={() => props.whenBathroomClicked(props.id)}
-			>
-				<Card style={{ width: '18rem' }} bg="light" text="light">
-					<Card.Header className="text-muted">Bathroom</Card.Header>
-					<Card.Body>
+			{/* <div onClick={() => props.whenBathroomClicked(props.id)}> */}
+			<Card className={classes.root} variant="outlined">
+				<CardContent>
+					<Typography
+						className={classes.title}
+						color="textSecondary"
+						gutterBottom
+					>
+						Bathroom
+					</Typography>
+					<Typography variant="h5" component="h2">
+						{props.name}
+					</Typography>
+					<Typography variant="body2" component="p">
+						{props.street} <br /> {props.upvote} upvotes{' '}
+					</Typography>
+				</CardContent>
+				<CardActions>
+					<Button size="lg" onClick={() => props.whenBathroomClicked(props.id)}>
+						{' '}
+						See Details{' '}
+					</Button>
+				</CardActions>
+				{/* <Card.Body>
 						<Card.Title className="text-muted">{props.name}</Card.Title>
 						<Card.Subtitle className="mb-2 text-muted">
 							{props.street}
@@ -23,9 +65,9 @@ function Bathroom(props) {
 						<Card.Text className="text-muted">
 							{props.upvote} upvotes{' '}
 						</Card.Text>
-					</Card.Body>
-				</Card>
-			</div>
+					</Card.Body> */}
+			</Card>
+			{/* </div> */}
 			<hr />
 		</React.Fragment>
 	);
